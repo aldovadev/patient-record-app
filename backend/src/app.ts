6a9b102976274bdafe033cc0baa2d530922e4c2f
aldovadev <aldova811@gmail.com>
2024-02-26 05:15:07 +0700

    feat(frontend) : init frontend cra typescript with cakraui

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
